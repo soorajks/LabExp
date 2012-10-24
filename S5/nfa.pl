7b5ffb4d65fb1bb49d217759fd3387248004fc9a
sooraj <soorajsooriadas@gmail.com>
2012-10-24 20:23:48 +0530

    Check NFA accept the given String

--- S5/nfa.pl
+++ S5/nfa.pl
@@ -1,13 +1,19 @@
-gcd(X,X,X).
-gcd(A,B,C):-
-	A>B,
-	D is A-B,
-	gcd(D,B,C).
-gcd(A,B,C):-
-	A<B,
-	D is B-A,
-	gcd(A,D,C).
-main(A,B):-
-	gcd(A,B,C),
-	write(' Result : '),
-	write(C).
+final(q3).
+tran(q0,_,q0).
+tran(q0,a,q1).
+tran(q1,b,q3).
+tran(q3,b,q2).
+epsilon(q3,q0).
+epsilon(q1,q2).
+accepts(State,[ ]):-
+	final(State),
+	nl,
+	write('String Accepted').
+accepts(State,[X|Rest]):-
+	tran(State,X,State1),
+	accepts(State1,Rest).
+accepts(State,[X|Rest]):-
+	epsilon(State,State1),
+	accepts(State1,Rest).
+main(A):-
+	accepts(q0,A).
